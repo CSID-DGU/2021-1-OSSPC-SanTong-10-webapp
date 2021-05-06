598a7345a9eadd6c7d0f4d7354b6d733d008659f
Youn <youn@Younui-MacBookPro.local>
2021-05-07 07:28:33 +0900

    init by taekwon

---
+++ src/main/java/com/dguossp/santong/SantongApplication.java
@@ -0,0 +1,13 @@
+package com.dguossp.santong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantongApplication.class, args);
+    }
+
+}
