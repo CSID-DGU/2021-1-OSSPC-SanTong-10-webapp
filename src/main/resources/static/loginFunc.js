8b92ca88ed6ce5188b8fdadb730fcefdd984f200
youeun <80808856+youeunss@users.noreply.github.com>
2021-05-28 13:05:01 +0900

    modified login/signup/doublechk API

--- src/main/resources/static/loginFunc.js
+++ src/main/resources/static/loginFunc.js
@@ -1,11 +1,27 @@
 function init(){
     var btnLogin = document.querySelector("#btnLogin");
     var form = document.querySelector("form");
-    form.addEventListener("submit",function(){
-        var requestURL = "/api/auth/login";
-        this.action = requestURL;
-        this.method='post';
-        this.submit();
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        fetch('/api/auth/login', {
+            method: 'POST',
+            body: new URLSearchParams(new FormData(event.target)),
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            console.log(data);
+            // 로그인 성공
+            if (data?.statusCode == 200) {
+                window.location = "index.html"
+            } else { // 로그인 실패
+                alert(data.message);
+            }
+        }).catch(function (error) {
+            console.warn(error);
+        });
     });
 }
 //window의 load이벤트 발생 감시
--- src/main/resources/static/signupFunc.js
+++ src/main/resources/static/signupFunc.js
@@ -1,7 +1,5 @@
 function invalidCheck(){
-    //dom트리에서 name속성값이 nickname인 객체찾기
     var inputId = document.querySelector("input[name=nickname]");
-    //dom트리에서 name속성값이 password인 객체찾기
     var inputPwd = document.querySelector("input[name=password]");
 
     if(inputId.value.length > 10){
@@ -10,9 +8,9 @@ function invalidCheck(){
         return false;
     }
 
-    var rePwd1 = /\W/; //특수문자용 정규식
-    var rePwd2 = /\w/; //문자와숫자,_용 정규식
-    //정규식 확인 메서드 : test()
+    var rePwd1 = /[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]/; //특수문자용 정규식
+    var rePwd2 = /[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]/; //문자와숫자,_용 정규식
+    //정규식 확인
     if(!rePwd1.test(inputPwd.value)){
         alert("비밀번호는 특수문자를 포함하세요.");
         inputPwd.focus();
@@ -25,3 +23,74 @@ function invalidCheck(){
     }
     return true;
 }
+
+function fnCheckDuplicateNickname() {
+    const nickname = document.querySelector("input[name=nickname]").value;
+    fetch('api/auth/nickname-check/'+nickname).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log(data);
+        // 닉네임 중복확인 성공
+        if (data?.statusCode == 1) {
+            alert(data?.message);
+        } else { // 닉네임 중복확인 실패
+            alert(data.message);
+        }
+    }).catch(function (error) {
+        console.warn(error);
+    });
+}
+
+function fnSignUp() {
+    const nickname = document.querySelector("input[name=nickname]");
+    fetch('api/auth/nickname-check', {
+        method: 'GET',
+        body: new URLSearchParams({nickname}),
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log(data);
+        // 닉네임 중복확인 성공
+        if (data?.statusCode == 1) {
+            alert(data?.message);
+        } else { // 닉네임 중복확인 실패
+            alert(data.message);
+        }
+    }).catch(function (error) {
+        console.warn(error);
+    });
+}
+
+function init(){
+    var form = document.querySelector("#signup");
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        fetch('/api/auth/signup', {
+            method: 'POST',
+            body: new URLSearchParams(new FormData(event.target)),
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        }).then(function (data) {
+            console.log(data);
+            // 성공
+            if (data?.statusCode == 200) {
+                window.location = "account/login.html"
+            } else { // 실패
+                alert(data.message);
+            }
+        }).catch(function (error) {
+            console.warn(error);
+        });
+    });
+}
+//window의 load이벤트 발생 감시
+window.addEventListener("load", init);
