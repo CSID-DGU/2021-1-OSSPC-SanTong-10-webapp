762d7611777bbe46f4abdf05ce1b1eaaf2694993
youeun <80808856+youeunss@users.noreply.github.com>
2021-05-31 07:50:32 +0900

    중복체크 기능 확인

--- src/main/resources/static/signupFunc.js
+++ src/main/resources/static/signupFunc.js
@@ -41,6 +41,8 @@ function fnCheckDuplicateNickname() {
         console.log(data);
         // 닉네임 중복확인 성공
         if (data?.statusCode == 1) {
+            //중복확인 체크 여부
+            document.querySelector("#checkYN").value = "Y";
             alert(data?.message);
         } else { // 닉네임 중복확인 실패
             alert(data.message);
@@ -50,7 +52,7 @@ function fnCheckDuplicateNickname() {
     });
 }
 
-
+/*
 function fnSignUp() {
     const nickname = document.querySelector("input[name=nickname]");
     //JSON 형식으로 요청
@@ -81,21 +83,40 @@ function fnSignUp() {
         console.warn(error);
     });
 }
+*/
 
 function init(){
     var form = document.querySelector("#signup");
     var nickname = document.getElementById("loginId").value;
     var password = document.getElementById("loginPw").value;
     var level = document.querySelector("input[name=level]:checked").value;
+    var passwordConfirm = document.getElementById("loginPwCheck").value;
 
     //JSON 요청
     var signupJson = new Object();
     signupJson.nickname = nickname;
+    //signupJson.user_id = nickname; 확인 후 수정
     signupJson.password = password;
     signupJson.level = level;
-
+//
     form.addEventListener("submit",function(event){
         event.preventDefault();
+
+        // 비밀번호 입력값과 비밀번호 확인값을 비교하는 로직
+        if (!(!password && !passwordConfirm && password === passwordConfirm)) {
+            alert("비밀번호와 비밀번호 확인 값이 다릅니다. 다시 확인해 주세요.");
+            return;
+        }
+
+        var checkYn = document.querySelector("#checkYN").value;
+        //중복확인 여부 체크
+        if (checkYn != "Y") {
+            alert("ID 중복 체크를 하셔야 합니다.");
+            return;
+        } else {
+            document.querySelector("#checkYN").value = "N";
+        }
+
         fetch('/api/auth/signup', {
             method: 'POST',
             headers: {
@@ -109,6 +130,7 @@ function init(){
             console.log(data);
             // 성공
             if (data?.statusCode == 200) {
+                alert("가입되었습니다.");
                 window.location = "/login"
             } else { // 실패
                 alert(data.message);
